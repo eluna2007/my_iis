3aded3d36dc3bec201a2515d10b894fe55b5c285
eluna2007 <eluna2007@gmail.com>
2018-05-10 07:19:07 -0400

    Update default_test.rb

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -1,9 +1,18 @@
 # # encoding: utf-8
 
-# Inspec test for recipe my_iis::default
+# Inspec test for recipe drupal::lamp
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-# For windows platforms make to the MS feature names not package names
-# To view all, go to windows powershell c:\get-windowsfeature
+unless os.windows?
+  # This is an example test, replace with your own test.
+  describe user('root'), :skip do
+    it { should exist }
+  end
+end
+
+# This is an example test, replace it with your own test.
+describe port(80) do
+  it { should be_listening }
+end
